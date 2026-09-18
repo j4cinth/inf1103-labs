b7cb4c69dcf2fc877f1dd7320dbf493bb5ffa87a
Jacinth <2604333@sit.singaporetech.edu.sg>
2026-09-18 16:44:54 +0800

    Lab3: Added get_valid_input function

--- week3/modular_auditor.py
+++ week3/modular_auditor.py
@@ -1,32 +1,42 @@
 # Week 2, Lab 2
 
+def get_valid_input():
+
+    while True:
+        user_input = input("Enter stock quantity (or type 'quit' to exit): ").strip()
+        # Check if user wants to quit
+        if user_input.lower() == 'quit':
+            return 'quit'
+        
+         # 4. Handle invalid input: Reject letters and punctuation
+        elif not user_input.isdigit() and not (user_input.startswith("-") and user_input[1:].isdigit()):
+            print("Error: Invalid entry. Please enter a valid integer.")
+            return 'invalid'
+        else:
+            stock_value = int(user_input)
+            # 5. Enforce business rules: Reject negative numbers
+            if stock_value < 0:
+                    print("Error: Negative values are not allowed.")
+                    return 'invalid'
+            else:
+                return stock_value
+
 # 1. Initialize variables
 total_inventory = 0
 failed_entries = 0
 
 # 2. Continuous loop until user types 'quit'
 while True:
-    user_input = input("Enter stock quantity (or type 'quit' to exit): ").strip()
-    
-    # Check if user wants to quit
-    if user_input.lower() == 'quit':
+    result = get_valid_input()
+
+    if  result == 'quit':
         break
-          
-    # 4. Handle invalid input: Reject letters and punctuation
-    # (But let negative numbers through by checking for the minus sign)
-    if not user_input.isdigit() and not (user_input.startswith("-") and user_input[1:].isdigit()):
-        print("Error: Invalid entry. Please enter a valid integer.")
-        failed_entries += 1
-        continue  # Move to the next iteration
-        
-    # Convert string to integer (safely works for negative digits now!)
-    stock_value = int(user_input)
-    
-    # 5. Enforce business rules: Reject negative numbers
-    if stock_value < 0:
-        print("Error: Negative values are not allowed.")
+    elif result == 'invalid':
         failed_entries += 1
         continue
+    
+    stock_value = result
+
 
     # 6. Manage State: Add to running total
     total_inventory += stock_value
@@ -41,3 +51,5 @@ while True:
 print("\n--- Audit Report ---")
 print(f"Total Units Processed: {total_inventory}")
 print(f"Number of Failed/Rejected Entries: {failed_entries}")
+
+    
